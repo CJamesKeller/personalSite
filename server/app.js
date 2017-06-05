6c4e2c9fd4f416b20b762b9d880f065b4ec46406
Chris Keller <cjameskeller@gmail.com>
2017-06-05 00:41:47 -0500

    fixing

--- server/app.js
+++ server/app.js
@@ -2,11 +2,17 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var bodyParser = require('body-parser');
-var port = 5000;
 
+app.set("port", (process.env.PORT || 5000));
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('server/public/views/index.html'));
 });
+
+app.listen(port, function() {
+  console.log("Listening on port: ", port);
+});
+
+module.exports = app;
